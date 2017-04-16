a79d50d62c3e9bd9a401306841f7214ebc901a02
Jeff <rustyangel@gmail.com>
2017-04-16 14:35:43 -0600

    ✨ Add SVG properties; closes #4

--- index.js
+++ index.js
@@ -256,6 +256,43 @@ module.exports = {
       "-ms-interpolation-mode",
 
 
+      // SVG Presentation Attributes.
+      "alignment-baseline",
+      "baseline-shift",
+      "dominant-baseline",
+      "text-anchor",
+      "word-spacing",
+      "writing-mode",
+
+      "fill",
+      "fill-opacity",
+      "fill-rule",
+      "stroke",
+      "stroke-dasharray",
+      "stroke-dashoffset",
+      "stroke-linecap",
+      "stroke-linejoin",
+      "stroke-miterlimit",
+      "stroke-opacity",
+      "stroke-width",
+
+      "color-interpolation",
+      "color-interpolation-filters",
+      "color-profile",
+      "color-rendering",
+      "flood-color",
+      "flood-opacity",
+      "image-rendering",
+      "lighting-color",
+      "marker-start",
+      "marker-mid",
+      "marker-end",
+      "mask",
+      "shape-rendering",
+      "stop-color",
+      "stop-opacity",
+
+
       // Transitions & Animation.
       "-webkit-transition",
       "-moz-transition",
