478c53c4dbddcb2f8a177219f1e2f1d070c9d94d
Jeff <rustyangel@gmail.com>
2018-12-14 21:13:15 -0700

    Add example code fixtures & update tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,28 +1,32 @@
 import test from 'ava'
 import stylelint from 'stylelint'
 
+import { correctOrder, incorrectOrder } from './_fixtures'
 import config from '..'
 
 const runStylelint = async (code) => {
-    const data = await stylelint.lint({
+    let data = await stylelint.lint({
         code,
         config,
     })
 
-    return data.results
+    return data.results[0]
 }
 
-test('property order enforced', async (t) => {
-    await runStylelint(`
-        div {
-            display: block;
-            position: relative;
-        }
-    `).then(([output]) => {
-        t.is(output.errored, true)
+test('with incorrect property order', async (t) => {
+    await runStylelint(incorrectOrder).then((output) => {
+        t.truthy(output.errored, 'indicates linting errors')
         t.is(
             output.warnings[0].text,
-            'Expected "position" to come before "display" (order/properties-order)',
+            'Expected "box-sizing" to come before "background-color" (order/properties-order)',
+            'indicates a properties-order error',
         )
+    })
+})
+
+test('with correct property order', async (t) => {
+    await runStylelint(correctOrder).then((output) => {
+        t.falsy(output.errored, 'indicates no errors')
+        t.is(output.warnings.length, 0)
     })
 })
