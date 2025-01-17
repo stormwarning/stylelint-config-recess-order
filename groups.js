fcc4ae522f95cbcb2915678c797b448557d15612
Jeff <rustyangel@gmail.com>
2025-01-16 20:51:59 -0700

    Clean up Animations group

--- groups.js
+++ groups.js
@@ -726,11 +726,11 @@ const propertyGroups = [
 			'animation',
 			'animation-name',
 			'animation-duration',
-			'animation-play-state',
 			'animation-timing-function',
 			'animation-delay',
 			'animation-iteration-count',
 			'animation-direction',
+			'animation-play-state',
 		],
 	},
 
